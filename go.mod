26e3aff89048aa322145acda56b863c6446544e3
Connor Demille <subtlepseudonym@gmail.com>
2019-02-12 22:10:53 -0500

    Fix uuid dep, fix uuid err typo, use args in uuid

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/subtlepseudonym/utilities
 
-require github.com/urfave/cli v1.20.0
+require (
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+	github.com/urfave/cli v1.20.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- uuid/main.go
+++ uuid/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/satori/go.uuid"
 )
 
 func main() {
-	n := flag.Int("n", 1, "number of UUID to generate")
-	flag.Parse()
+	n := 1
+	if len(os.Args) > 0 {
+		n64, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			_, e := os.Stderr.WriteString(fmt.Sprintf("unable to parse %q", os.Args[1]))
+			if e != nil {
+				panic(e)
+			}
+			os.Exit(1)
+		}
 
-	for i := 0; i < *n; i++ {
+		n = int(n64)
+	}
+
+	for i := 0; i < n; i++ {
 		id, err := uuid.NewV4()
 		if err != nil {
 			_, e := os.Stderr.WriteString("generate uuid failed: " + err.Error())
-			if err != nil {
+			if e != nil {
 				panic(e)
 			}
 			os.Exit(1)
