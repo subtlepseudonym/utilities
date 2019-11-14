5e3702d739aa0178bcb2afa3a89b8e7608f56d36
Connor Demille <subtlepseudonym@gmail.com>
2019-11-14 15:51:20 -0500

    Add version tag utility
    This is intended to replace build-tag.sh from subtlepsuedonym/notes

---
+++ vtag/main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/urfave/cli"
+)
+
+const (
+	cliGit = "cli"
+	goGit = "go-git"
+	libGit2 = "libgit2"
+
+	branchRegex = `.*-rc`
+	tagNameRegex = `tags.*\^0`
+)
+
+var Version = "0.0.0"
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "vtag"
+	app.Version = Version
+	app.Usage = "generate version tag for code releases"
+
+	app.Writer = os.Stdout
+	app.ErrWriter = os.Stderr
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name: "ignore-rc",
+			Usage: "don't use \"*-rc\" branch names as version",
+		},
+		cli.StringFlag{
+			Name: "git-lib",
+			Usage: "method for retrieving git repo info",
+			Value: "cli",
+		},
+	}
+
+	app.Action = versionTag
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func versionTag(ctx *cli.Context) error {
+	lib := ctx.String("git-lib")
+	switch lib {
+	case cliGit:
+		return cliAction(ctx)
+	case goGit:
+		return goGitAction(ctx)
+	case libGit2:
+		return libGit2Action(ctx)
+	default:
+		_, err := fmt.Fprintf(ctx.App.ErrWriter, "unrecognized git lib %q: use %q, %q, %q", lib, cliGit, goGit, libGit2)
+		return err
+	}
+}
+
+func buildTag(shortRevision []byte, added, deleted, updated int) string {
+	builder := new(strings.Builder)
+	var buildAdded bool
+	var changesAdded bool
+
+	if len(shortRevision) > 0 {
+		fmt.Fprintf(builder, "+%s", shortRevision)
+		buildAdded = true
+	}
+
+	builder, buildAdded, changesAdded = addChange(builder, 'a', added, buildAdded, changesAdded)
+	builder, buildAdded, changesAdded = addChange(builder, 'd', deleted, buildAdded, changesAdded)
+	builder, buildAdded, changesAdded = addChange(builder, 'u', updated, buildAdded, changesAdded)
+
+	return builder.String()
+}
+
+func addChange(builder *strings.Builder, tag rune, count int, build, changes bool) (*strings.Builder, bool, bool) {
+	if count == 0 {
+		return builder, build, changes
+	}
+
+	if !build {
+		builder.WriteRune('+')
+	} else if !changes {
+		builder.WriteRune('.')
+	}
+	fmt.Fprintf(builder, "%c%d", tag, count)
+
+	return builder, true, true
+}
+
+func cliAction(ctx *cli.Context) error {
+	versionTagCmd := exec.Command("git", "describe", "--abbrev=0")
+	tag, err := versionTagCmd.Output()
+	if err != nil {
+		return fmt.Errorf("get version: %w", err)
+	}
+	version := tag
+
+	if !ctx.Bool("ignore-rc") {
+		branchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+		branch, err := branchCmd.Output()
+		if err != nil {
+			return fmt.Errorf("get branch: %w", err)
+		}
+
+		if regexp.MustCompile(branchRegex).Match(branch) {
+			version = branch
+		}
+	}
+	version = bytes.Trim(version, "\n")
+
+	revNameCmd := exec.Command("git", "name-rev", "--name-only", "HEAD")
+	revName, err := revNameCmd.Output()
+	if err != nil {
+		return fmt.Errorf("get rev name: %w", err)
+	}
+
+	var revision []byte
+	if !regexp.MustCompile(tagNameRegex).Match(revName) {
+		shortRevCmd := exec.Command("git", "rev-list", "-n1", "--abbrev-commit", "HEAD")
+		shortRev, err := shortRevCmd.Output()
+		if err != nil {
+			return fmt.Errorf("get short rev: %w", err)
+		}
+		revision = bytes.Trim(shortRev, "\n")
+	}
+
+	var added, deleted int
+	diffStatCmd := exec.Command("git", "diff-files", "--numstat")
+	diffStatOutput, err := diffStatCmd.Output()
+	if err != nil {
+		return fmt.Errorf("get diff stat: %w", err)
+	}
+
+	for _, line := range strings.Split(string(diffStatOutput), "\n") {
+		if line == "" {
+			continue
+		}
+		stats := strings.Split(line, "\t")
+
+		addCount, err := strconv.Atoi(stats[0])
+		if err != nil {
+			return fmt.Errorf("parse int %q: %w", stats[0], err)
+		}
+		added += addCount
+
+		delCount, err := strconv.Atoi(stats[1])
+		if err != nil {
+			return fmt.Errorf("parse int %q: %w", stats[1], err)
+		}
+		deleted += delCount
+	}
+
+	lsFilesCmd := exec.Command("git", "ls-files", "--others", "--exclude-standard")
+	lsFilesOutput, err := lsFilesCmd.Output()
+	if err != nil {
+		return fmt.Errorf("get untracked: %w", err)
+	}
+	updated := len(strings.Split(string(lsFilesOutput), "\n")) - 1
+
+	_, err = fmt.Fprintf(ctx.App.Writer, "%s%s", version, buildTag(revision, added, deleted, updated))
+	return err
+}
+
+func goGitAction(ctx *cli.Context) error {
+	_, err := fmt.Fprintf(ctx.App.Writer, "not implemented")
+	return err
+}
+
+func libGit2Action(ctx *cli.Context) error {
+	_, err := fmt.Fprintf(ctx.App.Writer, "not implemented")
+	return err
+}
