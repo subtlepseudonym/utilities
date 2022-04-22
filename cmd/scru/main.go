a16f9ea5ebb6bd2e37e06ee28c8760449394297e
Connor Demille <subtlepseudonym@gmail.com>
2022-04-21 21:13:51 -0400

    Add utility for generating SCRU128 IDs

---
+++ cmd/scru/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/scru128/go-scru128"
+	"github.com/spf13/cobra"
+)
+
+var Version = "1.0.0"
+
+func main() {
+	cmd := cobra.Command{
+		Use:       "scru [count]",
+		Short:     "Generate SCRU128 IDs",
+		ValidArgs: []string{"count"},
+		Version:   Version,
+		RunE:      run,
+	}
+
+	err := cmd.Execute()
+	if err != nil {
+		fmt.Println("ERR:", err)
+		os.Exit(1)
+	}
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	err := cmd.ParseFlags(args)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	count := 1
+	if len(args) > 0 {
+		count, err = strconv.Atoi(args[0])
+		if err != nil {
+			return fmt.Errorf("parse count arg: %w", err)
+		}
+	}
+
+	generator := scru128.NewGenerator()
+	for i := 0; i < count; i++ {
+		id, err := generator.Generate()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(id)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/subtlepseudonym/utilities
 require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+	github.com/scru128/go-scru128 v1.0.0
+	github.com/spf13/cobra v1.4.0
 	github.com/subtlepseudonym/version v0.1.0
 	github.com/urfave/cli v1.22.2
 	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
