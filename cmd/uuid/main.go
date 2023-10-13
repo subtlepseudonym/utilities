d8ac60a7ccd29d98b310b8eee501e584e10e5977
Connor Demille <subtlepseudonym@gmail.com>
2023-10-13 16:01:58 -0400

    Swap uuid from urfave/cli to spf13/cobra

--- cmd/uuid/main.go
+++ cmd/uuid/main.go
@@ -4,131 +4,115 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/gofrs/uuid/v5"
-	"github.com/urfave/cli"
+	"github.com/spf13/cobra"
 )
 
 var Version = "1.1.0"
 
 var (
-	uuidType  byte // uuid.V1, uuid.V3, etc
-	namespace uuid.UUID
+	count    int
+	uuidType string
 )
 
 func main() {
-	app := cli.NewApp()
+	cmd := cobra.Command{
+		Use:          "uuid [flags] [count]",
+		Short:        "Generate UUIDs",
+		ValidArgs:    []string{"count"},
+		Version:      Version,
+		RunE:         run,
+		SilenceUsage: true,
+	}
 
-	app.Name = "uuid"
-	app.Version = Version
-	app.Usage = "generates v4 UUIDs"
-	app.UsageText = fmt.Sprintf("%s [flags] [count]", app.Name)
-	app.Description = `Generates UUIDs.
+	cmd.Long = `Generates UUIDs.
 
-	Supported UUID types:
-		- [1|v1]:            Version 1 (date-time and MAC address)
-		- [3|v3|md5]:        Version 3 (namespace name-based MD5)
-		- [4|v4|random]:     Version 4 (random)
-		- [5|v5|sha1|sha-1]: Version 5 (namespace name-based SHA-1)
-		- [6|v6]:            Version 6 (k-sortable and random, field-compatible with v1)
-		- [7|v7]:            Version 7 (k-sortable and random)
+Supported types:
+ [1|v1]:            Version 1 (date-time and MAC address)
+ [3|v3|md5]:        Version 3 (namespace name-based MD5)
+ [4|v4|random]:     Version 4 (random)
+ [5|v5|sha1|sha-1]: Version 5 (namespace name-based SHA-1)
+ [6|v6]:            Version 6 (k-sortable and random, field-compatible with v1)
+ [7|v7]:            Version 7 (k-sortable and random)
 `
 
-	app.Writer = os.Stdout
-	app.ErrWriter = os.Stderr
+	cmd.Flags().IntVarP(&count, "count", "n", 1, "number of UUIDs to generate")
+	cmd.Flags().StringVarP(&uuidType, "type", "t", "v4", "type of UUID")
+	cmd.Flags().String("namespace", "", "namespace for UUID v3 and v5")
+	cmd.Flags().String("name", "", "name for UUID v3 and v5")
+	cmd.ParseFlags(os.Args[1:])
 
-	app.Flags = []cli.Flag{
-		cli.IntFlag{
-			Name:  "count, n",
-			Usage: "number of UUIDs to generate",
-			Value: 1,
-		},
-		cli.StringFlag{
-			Name:  "type, t",
-			Usage: "type of UUID",
-			Value: "v4",
-		},
-		cli.StringFlag{
-			Name:  "namespace",
-			Usage: "namespace for UUID v3 or v5",
-		},
-		cli.StringFlag{
-			Name:  "name",
-			Usage: "name for UUID v3 or v5",
-		},
-	}
-
-	app.Before = processArgs
-	app.Action = action
-
-	app.Authors = []cli.Author{
-		{
-			Name:  "Connor Demille",
-			Email: "subtlepseudonym@gmail.com",
-		},
-	}
-
-	err := app.Run(os.Args)
+	err := cmd.Execute()
 	if err != nil {
-		fmt.Fprintln(app.ErrWriter, err)
+		os.Exit(1)
 	}
 }
 
-func processArgs(ctx *cli.Context) (err error) {
-	if !ctx.IsSet("count") && ctx.NArg() > 0 {
-		countArg := ctx.Args()[0]
-		_, err = strconv.Atoi(countArg)
+func run(cmd *cobra.Command, args []string) error {
+	if !cmd.Flag("count").Changed && len(args) > 0 {
+		n, err := strconv.Atoi(args[0])
 		if err != nil {
-			return fmt.Errorf("parse count arg: %w", err)
+			return fmt.Errorf("parse arg as count: %w", err)
 		}
-		ctx.Set("count", countArg)
+		count = n
 	}
 
-	switch strings.ToLower(ctx.String("type")) {
+	// TODO: if we eventually support too many aliases, use a map[string]byte
+	var uuidVersion byte
+	switch uuidType {
 	case "1", "v1":
-		uuidType = uuid.V1
+		uuidVersion = uuid.V1
 	case "3", "v3", "md5":
-		uuidType = uuid.V3
+		uuidVersion = uuid.V3
 	case "4", "v4", "random":
-		uuidType = uuid.V4
+		uuidVersion = uuid.V4
 	case "5", "v5", "sha1", "sha-1":
-		uuidType = uuid.V5
+		uuidVersion = uuid.V5
 	case "6", "v6":
-		uuidType = uuid.V6
+		uuidVersion = uuid.V6
 	case "7", "v7":
-		uuidType = uuid.V7
+		uuidVersion = uuid.V7
 	default:
-		return fmt.Errorf("unknown UUID type: %q", ctx.String("type"))
+		return fmt.Errorf("unknown UUID type: %q", uuidType)
 	}
 
-	if uuidType == uuid.V3 || uuidType == uuid.V5 {
-		namespace, err = uuid.FromString(ctx.String("namespace"))
+	var namespace uuid.UUID
+	var name string
+	if uuidVersion == uuid.V3 || uuidVersion == uuid.V5 {
+		if !cmd.Flag("namespace").Changed {
+			return fmt.Errorf("namespace is required for UUID v3 and v5")
+		}
+		ns, err := cmd.Flags().GetString("namespace")
+		if err != nil {
+			return fmt.Errorf("get namespace flag: %w", err)
+		}
+		namespace, err = uuid.FromString(ns)
 		if err != nil {
 			return fmt.Errorf("parse namespace UUID: %w", err)
 		}
 
-		if !ctx.IsSet("name") {
+		if !cmd.Flag("name").Changed {
 			return fmt.Errorf("name is required for UUID v3 and v5")
+		}
+		name, err = cmd.Flags().GetString("name")
+		if err != nil {
+			return fmt.Errorf("get name flag: %w", err)
 		}
 	}
 
-	return nil
-}
-
-func action(ctx *cli.Context) error {
-	for i := 0; i < ctx.Int("count"); i++ {
+	for i := 0; i < count; i++ {
 		var id uuid.UUID
 		var err error
-		switch uuidType {
+		switch uuidVersion {
 		case uuid.V1:
 			id, err = uuid.NewV1()
 		case uuid.V3:
-			id = uuid.NewV3(namespace, ctx.String("name"))
+			id = uuid.NewV3(namespace, name)
 		case uuid.V4:
 			id, err = uuid.NewV4()
 		case uuid.V5:
-			id = uuid.NewV5(namespace, ctx.String("name"))
+			id = uuid.NewV5(namespace, name)
 		case uuid.V6:
 			id, err = uuid.NewV6()
 		case uuid.V7:
@@ -141,7 +125,7 @@ func action(ctx *cli.Context) error {
 		if err != nil {
 			return fmt.Errorf("new uuid: %v", err)
 		}
-		fmt.Fprintln(ctx.App.Writer, id.String())
+		fmt.Println(id.String())
 	}
 
 	return nil
